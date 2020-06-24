75d55a667ba8cff453286615a8c92041e641564e
sampathkumar <semsampathkumar@gmail.com>
2020-06-24 00:30:43 -0700

    Datatypes bool,char and string

--- app/src/main/java/eu/tutorial/initialproject/KotlinBasics.kt
+++ app/src/main/java/eu/tutorial/initialproject/KotlinBasics.kt
@@ -1,21 +1,41 @@
 package eu.tutorial.initialproject
 
 fun main(){
+    //1.
     //variable  declare by var and val
     // val - immutable variable
     // var - muttable variable
     var name = "sampath"
     name = "kumar"
-    print("my name is " + name)
+//    print("my name is " + name)
 
-
-    //Integer Types: Byte(8 bit), Short(16 bit), Int(32 bit), Long(64 bit)
+    //2.
+    // Integer Types: Byte(8 bit), Short(16 bit), Int(32 bit), Long(64 bit)
     val byte: Byte = 44
     val short: Short = 155
     val  int :Int = 1564654
     val long : Long = 11_46_498_497_899_789_897
 
-    //Floating point number types: Float(32 bit) Double(64 bit)
+    // Floating point number types: Float(32 bit) Double(64 bit)
     val float: Float = 13.45F
     val double: Double = 13.4864544564656
+
+    //3.
+    // boolean is used to represented logical values.
+    // two possible values and false
+
+    //boolean
+    var isWinter: Boolean = true
+    isWinter = false
+
+    //characters
+    val letterChar =  'h'
+    val digitChar = '8'
+
+    //string
+    val team = "sgp stars"
+    var firstChar = team[3]
+    var lastChar = team[team.length -1]
+    print("last character " + lastChar)
+
 }
