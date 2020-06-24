1ee504c6d6b1a13e7cdbe5bb6182d517ff1a9af7
sampathkumar <semsampathkumar@gmail.com>
2020-06-23 23:29:23 -0700

    variables and difference between val and var

---
+++ app/src/main/java/eu/tutorial/initialproject/KotlinBasics.kt
@@ -0,0 +1,12 @@
+package eu.tutorial.initialproject
+
+fun main(){
+    //variable  declare by var and val
+    // val - immutable variable
+    // var - muttable variable
+
+    var name = "sampath"
+    name = "kumar"
+    print("my name is " + name)
+
+}
