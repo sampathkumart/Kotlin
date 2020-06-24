8ec89520cea1103fce86d3c4adbae1a53fc5e960
sampathkumar <semsampathkumar@gmail.com>
2020-06-23 23:59:35 -0700

    Datatypes of numbers

--- app/src/main/java/eu/tutorial/initialproject/KotlinBasics.kt
+++ app/src/main/java/eu/tutorial/initialproject/KotlinBasics.kt
@@ -4,9 +4,18 @@ fun main(){
     //variable  declare by var and val
     // val - immutable variable
     // var - muttable variable
-
     var name = "sampath"
     name = "kumar"
     print("my name is " + name)
 
+
+    //Integer Types: Byte(8 bit), Short(16 bit), Int(32 bit), Long(64 bit)
+    val byte: Byte = 44
+    val short: Short = 155
+    val  int :Int = 1564654
+    val long : Long = 11_46_498_497_899_789_897
+
+    //Floating point number types: Float(32 bit) Double(64 bit)
+    val float: Float = 13.45F
+    val double: Double = 13.4864544564656
 }
